34666440d459713ecd655293ef01bdf4d6251b39
AndresVlz2020 <santyguitar6@gmail.com>
2026-05-11 09:22:50 -0500

    Nuevos cambios añadidos

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App () {
+  return (
+    <div className="min-h-screen bg-green-800 flex item-center justify-center">
+      <h1 className="text-white text-4x1 font-bold">
+        Con Rico Programar Tailwind V4 funciona full...
+      </h1>
+    </div>
+  );
+}
